e4f4317136c1f563001be28a1c4ba31c9deea9f7
bcbell <coleman.britt@gmail.com>
2021-02-11 10:58:07 -0500

    Deploy

--- wom_nology/settings.py
+++ wom_nology/settings.py
@@ -51,10 +51,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'crispy_forms',
+    # 'crispy_forms',
     'ckeditor',
     
 ]
+
 
 
 MIDDLEWARE = [
