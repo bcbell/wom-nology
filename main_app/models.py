fb0e671db969bc4c98a75386cc7f6c2a714a588e
bcbell <coleman.britt@gmail.com>
2021-02-11 11:56:11 -0500

    Deploy

--- main_app/models.py
+++ main_app/models.py
@@ -169,10 +169,10 @@ class Profile(models.Model):
     discussions=models.ManyToManyField(Discussion, related_name='posts')
     replies= models.ManyToManyField(Reply, related_name='replies')
     # avatar=models.ForeignKey(Photo, on_delete=models.CASCADE, related_name='pic', null=True)
-    linkedin_url=models.CharField(max_length=255, null=True, blank=True)
-    facebook_url=models.CharField(max_length=255, null=True, blank=True)
-    twitter_url=models.CharField(max_length=255, null=True, blank=True)
-    website_url=models.CharField(max_length=255, null=True, blank=True)
+    # linkedin_url=models.CharField(max_length=255, null=True, blank=True)
+    # facebook_url=models.CharField(max_length=255, null=True, blank=True)
+    # twitter_url=models.CharField(max_length=255, null=True, blank=True)
+    # website_url=models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
